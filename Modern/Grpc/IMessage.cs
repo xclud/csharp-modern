160d503dff6f931a236b241a411296ebfd0be29b
Mahdi <mkfard@outlook.com>
2025-11-07 23:49:35 +0330

    Initial commit.

---
+++ Modern/Grpc/IMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Modern.Grpc;
+
+public interface IMessage
+{
+    void WriteTo(CodedBufferWriter writer);
+}
+
+public interface IMessage<T> : IMessage
+{
+    abstract static T Deserialize(byte[] bytes);
+}
